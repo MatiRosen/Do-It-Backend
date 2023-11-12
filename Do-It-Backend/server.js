332c347b01a6540562d95150eb1aa28f9e65c881
Francisco Lagares <60282514+Pancholag@users.noreply.github.com>
2023-11-12 18:07:45 -0300

    agrego endpoint para notificaciones

--- Do-It-Backend/server.js
+++ Do-It-Backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const axios = require('axios');
+const { google } = require('googleapis');
+const serviceAccount = require('./prf-do-it-firebase-adminsdk-u8ko1-d52a2c7ea6.json');
 const app = express();
 const stripe = require("stripe")('sk_test_51O2Oj4F9ihC2ndyiNWTZuJ74LgKhwJWSWBkqecP572fj2VY14He9f1LqAkQ4saUbIy2cHDBcD92FrJBddmIrfBIN00fa6gFlKE');
 const port = process.env.PORT || 3000;
@@ -29,5 +32,55 @@ app.post("/create-payment-intent", async (req, res) => {
 app.get("/greet", async(req, res) => {
     res.send("Server is working...");
 });
+
+app.post('/send-notification', async (req, res) => {
+  try {
+    const bearerToken = await generateBearerToken();
+
+    const {title, body, token} = req.body;
+
+    const notification = {
+      title,
+      body,
+    };
+
+    const requestData = {
+      message: {
+        token,
+        notification,
+      },
+    };
+
+    const response = await axios.post(
+      'https://fcm.googleapis.com/v1/projects/prf-do-it/messages:send',
+      requestData,
+      {
+        headers: {
+          Authorization: bearerToken,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+async function generateBearerToken() {
+  const scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
+  const auth = new google.auth.GoogleAuth({
+    credentials: serviceAccount,
+    scopes,
+  });
+
+  const client = await auth.getClient();
+  const tokenResponse = await client.getAccessToken();
+
+  const bearerToken = `Bearer ${tokenResponse.token}`;
+
+  return bearerToken;
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
